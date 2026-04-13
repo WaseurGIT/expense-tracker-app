894b2f1b6e53bf47e95741b222a4d6cbfd6f29cc
WaseurGIT <waseurrahman31@gmail.com>
2026-04-13 10:41:15 +0600

    tried login and registration authentication

--- app/axiosSecure.js
+++ app/axiosSecure.js
@@ -3,7 +3,6 @@ import axios from "axios";
 
 const axiosSecure = axios.create({
 //   baseURL: "https://expense-tracker-app-server-l1bm.onrender.com",
-  baseURL: "https://192.168.10.70:3000",
 });
 
 axiosSecure.interceptors.request.use(async (config) => {
